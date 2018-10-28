42bbc3df35b61690be79d91d78f75928df64f56a
xb212526 <p000tspereira@prservicos.com.br>
2018-10-28 10:30:02 -0300

    Foi criado os metodos de inserção, atualização,buscar por cpf e de listar todos

--- src/main/java/com/tiagopereira/autoescola/controller/AlunoController.java
+++ src/main/java/com/tiagopereira/autoescola/controller/AlunoController.java
@@ -1,14 +1,22 @@
 package com.tiagopereira.autoescola.controller;
 
-import com.tiagopereira.autoescola.entity.Aluno;
+import com.tiagopereira.autoescola.dto.AlunoDTO;
 import com.tiagopereira.autoescola.service.AlunoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import com.tiagopereira.autoescola.responses.Response;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
 
 @RestController
-@RequestMapping("/alunos")
+@RequestMapping(value = "/alunos")
 public class AlunoController {
 
     private final AlunoService alunoService;
@@ -18,8 +26,62 @@ public class AlunoController {
         this.alunoService = alunoService;
     }
 
-//    @GetMapping
-//    public Aluno buscar(){
-//       return alunoService
-//    }
+    @GetMapping(path = "/{cpf}")
+    public AlunoDTO buscar(@PathVariable("cpf") String cpf) {
+        return alunoService.findByCpf(cpf);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AlunoDTO>> listarAlunos() {
+        List<AlunoDTO> alunoDTO = alunoService.listar();
+        return ResponseEntity.status(HttpStatus.OK).body(alunoDTO);
+    }
+
+    @PostMapping(path = "/novoAluno")
+    public ResponseEntity<Response<AlunoDTO>> salvarAluno(@Valid @RequestBody AlunoDTO alunoDTO,
+                                                          BindingResult result) {
+        Response<AlunoDTO> response = new Response<AlunoDTO>() {
+        };
+        if (result.hasErrors()) {
+            result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+            return ResponseEntity.badRequest().body(response);
+        }
+
+        AlunoDTO alunoDTOSalva = this.alunoService.salvar(alunoDTO);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(alunoDTO.getCpf()).toUri();
+        response.setData(alunoDTOSalva);
+        return ResponseEntity.created(location).body(response);
+    }
+
+    @PutMapping(path = "/alterarAluno")
+    public ResponseEntity<Response<AlunoDTO>> alterarAluno(@RequestBody AlunoDTO alunoDTO, BindingResult result) {
+        Response<AlunoDTO> response = new Response<>();
+        try {
+            validateUpdateAluno(alunoDTO, result);
+            if (result.hasErrors()) {
+                result.getAllErrors().forEach(objectError -> response.getErrors().add(objectError.getDefaultMessage()));
+                return ResponseEntity.badRequest().body(response);
+            }
+
+            alunoDTO.setCpf(alunoDTO.getCpf());
+
+            AlunoDTO alunoAtualizado = this.alunoService.salvar(alunoDTO);
+
+            response.setData(alunoAtualizado);
+        } catch (Exception e) {
+            response.getErrors().add(e.getMessage());
+            return ResponseEntity.badRequest().body(response);
+        }
+        return ResponseEntity.ok(response);
+    }
+
+    private void validateUpdateAluno(AlunoDTO alunoDTO, BindingResult result) {
+        if (alunoDTO.getId() == null) {
+            result.addError(new ObjectError("Aluno", "Id do aluno não informado"));
+        }
+        if (alunoDTO.getCpf() == null) {
+            result.addError(new ObjectError("Aluno", "CPF do aluno não informado"));
+        }
+    }
+
 }
--- src/main/java/com/tiagopereira/autoescola/service/AlunoService.java
+++ src/main/java/com/tiagopereira/autoescola/service/AlunoService.java
@@ -1,11 +1,13 @@
 package com.tiagopereira.autoescola.service;
 
+import com.tiagopereira.autoescola.conversordto.AlunoConversorDTO;
 import com.tiagopereira.autoescola.dto.AlunoDTO;
 import com.tiagopereira.autoescola.entity.Aluno;
 import com.tiagopereira.autoescola.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,15 +21,41 @@ public class AlunoService {
         this.alunoRepository = alunoRepository;
     }
 
-    public List<Aluno> listar(){
-        return alunoRepository.findAll();
+    public List<AlunoDTO> listar(){
+        AlunoConversorDTO conversorDTO = new AlunoConversorDTO();
+        List<Aluno> alunos = alunoRepository.findAll();
+
+        List<AlunoDTO> lstDTO = new ArrayList<>();
+
+        for(Aluno aluno : alunos){
+            AlunoDTO dto = conversorDTO.converteEntidadeParaDTO(aluno);
+            lstDTO.add(dto);
+        }
+
+        return lstDTO;
     }
 
-//    public AlunoDTO findByCpf(String cpf){
-//        AlunoConversorDTO conversorDTO = new  AlunoConversoresDTO();
-//        Aluno aluno = this.alunoRepository.findByCpf(cpf);
-//        AlunoDTO alunoDTO = conversoresDTO.converteEntityParaDTO(aluno);
-//        return aluno;
-//    }
+    public AlunoDTO salvar(AlunoDTO alunoDTO){
+
+        Aluno aluno = null;
+        if(alunoDTO.getCpf() !=null && !alunoDTO.getCpf().equals((""))){
+            aluno = this.alunoRepository.findByCpf(alunoDTO.getCpf());
+        }
+
+        AlunoConversorDTO conversorDTO = new AlunoConversorDTO();
+        aluno = conversorDTO.converteDtoParaEntidade(alunoDTO, aluno);
+
+        Aluno alunoSalvo = alunoRepository.save(aluno);
+
+        return  conversorDTO.converteEntidadeParaDTO(alunoSalvo);
+    }
+
+    public AlunoDTO findByCpf(String cpf){
+        AlunoConversorDTO conversorDTO = new AlunoConversorDTO();
+        Aluno aluno = this.alunoRepository.findByCpf(cpf);
+        AlunoDTO alunoDTO = conversorDTO.converteEntidadeParaDTO(aluno);
+
+        return alunoDTO;
+    }
 
 }
